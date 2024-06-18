9906475a4e3309c0588a1e42bad7c25fbb7ba08c
Adam Gemmell <gemmell.adam+github@gmail.com>
2024-06-18 11:43:46 +0100

    Vim: Move to LazyVim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    opts = {},
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized",
+    },
+  },
+}
